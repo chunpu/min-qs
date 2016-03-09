66b39d366a7a28346aa93c11883d5139840eddd7
chunpu <527653908@qq.com>
2016-03-09 14:34:12 +0800

    m

--- _readme.md
+++ _readme.md
@@ -1,4 +1,5 @@
 Usage
+---
 
 just like querystring in node.js
 
@@ -12,4 +13,48 @@ No nested or array support, can also custom sep and eq
 
 ```js
 qs.parse('a:1;b:2', ';', ':')
+```
+
+Options
+---
+
+`keepRaw` if false will not do any encode or decode, default is true
+
+```js
+var ret = qs.stringify({
+	'&=': ' '
+}, {keepRaw: true})
+assert.deepEqual(ret, '&== ')
+```
+
+`sort` true or sort function
+
+```js
+var ret = qs.stringify({
+	c: 3,
+	a: 1,
+	b: 2,
+	aa: 11
+}, {
+	sort: true
+})
+assert.deepEqual(ret, 'a=1&aa=11&b=2&c=3')
+```
+
+`ignoreValues` ignore some values, default is `[undefined]`
+
+```js
+var ret = qs.stringify({
+	'0': 0,
+	'empty': '',
+	'NaN': NaN,
+	'null': null,
+	'undefined': undefined,
+	'false': false
+}, {
+	sort: true,
+	ignoreValues: [undefined, null]
+})
+
+assert.deepEqual(ret, '0=0&NaN=&empty=&false=false')
 ```
--- test.js
+++ test.js
@@ -34,6 +34,12 @@ describe('parse', function() {
 			assert.deepEqual(ret, {})
 		}
 	})
+
+	it('support keepRaw option to return raw string', function() {
+		var ret = qs.parse('%26%3D=%20', {keepRaw: true})
+		assert.deepEqual(ret, {'%26%3D': '%20'})
+	})
+
 })
 
 describe('stringify', function() {
@@ -45,12 +51,26 @@ describe('stringify', function() {
 		assert('a=1&b=foo' == ret || 'b=foo&a=1' == ret)
 	})
 
-	it('should encode both key and value', function() {
+	it('should encode and decode will get self', function() {
 		var dirty = {
 			'#': ' ',
 			b: '#$'
 		}
 		assert.deepEqual(dirty, qs.parse(qs.stringify(dirty)))
+	})
+
+	it('default will encode key and value', function() {
+		var ret = qs.stringify({
+			'&=': ' '
+		})
+		assert.deepEqual(ret, '%26%3D=%20')
+	})
+
+	it('support keepRaw option to not encode key and value', function() {
+		var ret = qs.stringify({
+			'&=': ' '
+		}, {keepRaw: true})
+		assert.deepEqual(ret, '&== ')
 	})
 
 	it('can custom sep and eq', function() {
@@ -68,4 +88,54 @@ describe('stringify', function() {
 			assert.deepEqual(ret, '')
 		}
 	})
+
+	it('support sort option', function() {
+		var ret = qs.stringify({
+			c: 3,
+			a: 1,
+			b: 2,
+			aa: 11
+		}, {
+			sort: true
+		})
+		assert.deepEqual(ret, 'a=1&aa=11&b=2&c=3')
+	})
+
+	it('support custom sort option', function() {
+		var ret = qs.stringify({
+			c: 3,
+			a: 1,
+			b: 2,
+			aa: 11,
+			bbb: 222
+		}, {
+			sort: function(a, b) {
+				a += ''
+				b += ''
+				if (a.length != b.length) {
+					return a.length - b.length
+				}
+				return a > b
+			}
+		})
+
+		assert.deepEqual(ret, 'a=1&b=2&c=3&aa=11&bbb=222')
+	})
+
+	it('can custom ignore values', function() {
+		var ret = qs.stringify({
+			'0': 0,
+			'empty': '',
+			'NaN': NaN,
+			'null': null,
+			'undefined': undefined,
+			'false': false
+		}, {
+			sort: true,
+			ignoreValues: [undefined, null]
+		})
+
+		assert.deepEqual(ret, '0=0&NaN=&empty=&false=false')
+	})
+
 })
